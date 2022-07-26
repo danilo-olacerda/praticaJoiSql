85b711e8924b60149793b49c8817c3b1f7945757
Danilo <dandam3006@gmail.com>
2022-07-26 20:23:14 -0300

    All questions

--- queries.sql
+++ queries.sql
@@ -21,7 +21,7 @@ SELECT users.id, users.name, roles.name AS role, companies.name AS company, expe
 JOIN users ON experiences."userId" = users.id
 JOIN roles ON experiences."roleId" = roles.id
 JOIN companies ON experiences."companyId" = companies.id
-WHERE users.id = 50 AND experiences."endDate"= null;
+WHERE users.id = 50 AND experiences."endDate" IS null;
 
 
 
